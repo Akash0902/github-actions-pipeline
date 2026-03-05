5b0cf0ec6fa8a678def6d06ddf653c490483f062
Akash0902 <115146885+Akash0902@users.noreply.github.com>
2026-03-05 16:28:52 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -34,7 +34,7 @@ resource "aws_route_table" "public_rt"{
 }
 
 #Associate Subnet 
-resource "aws_route_table_assoication" "public_assoc"{
+resource "aws_route_table_association" "public_assoc"{
   subnet_id = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
