5f4e64b6b3e40427a0b0b6df58bae95300a14d7e
Akash0902 <115146885+Akash0902@users.noreply.github.com>
2026-03-05 12:49:42 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,3 +1,4 @@
+#VPC
 resource "aws_vpc" "main_vpc"{
   cidr_block = "10.0.0.0/16"
 
@@ -5,3 +6,42 @@ resource "aws_vpc" "main_vpc"{
     Name = "devops-vpc"
 }
 }
+
+#Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id = aws_vpc.main_vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+}
+}
+
+#Internet Gateway
+resource = "aws_internet_gateway" "gw"{
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+#Route Table
+resource "aws_route_table" "public_rt"{
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+}
+}
+
+#Associate Subnet 
+resource "aws_route_table_assoication" "public_assoc"{
+  subnet_id = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+
+
+
+
+
+
