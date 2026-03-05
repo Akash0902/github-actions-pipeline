9f9d5d76be558d1c3bca03cd8e98b9ab1a22a4a3
Akash0902 <115146885+Akash0902@users.noreply.github.com>
2026-03-05 15:33:20 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,1 +1,121 @@
+############################
+# IAM ROLE FOR EKS CLUSTER
+############################
 
+resource "aws_iam_role" "eks_cluster_role" {
+
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+
+  role = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+
+}
+
+############################
+# EKS CLUSTER
+############################
+
+resource "aws_eks_cluster" "devops_cluster" {
+
+  name = "devops-eks-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.public_subnet.id
+    ]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy
+  ]
+}
+
+############################
+# NODE IAM ROLE
+############################
+
+resource "aws_iam_role" "eks_node_role" {
+
+  name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [{
+      Effect = "Allow"
+
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node_policy" {
+
+  role = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+
+}
+
+resource "aws_iam_role_policy_attachment" "cni_policy" {
+
+  role = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+
+}
+
+resource "aws_iam_role_policy_attachment" "registry_policy" {
+
+  role = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+
+}
+
+############################
+# EKS NODE GROUP
+############################
+
+resource "aws_eks_node_group" "devops_nodes" {
+
+  cluster_name = aws_eks_cluster.devops_cluster.name
+  node_group_name = "devops-node-group"
+  node_role_arn = aws_iam_role.eks_node_role.arn
+
+  subnet_ids = [
+    aws_subnet.public_subnet.id
+  ]
+
+  instance_types = ["t3.medium"]
+
+  scaling_config {
+    desired_size = 2
+    max_size = 3
+    min_size = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.worker_node_policy,
+    aws_iam_role_policy_attachment.cni_policy,
+    aws_iam_role_policy_attachment.registry_policy
+  ]
+}
