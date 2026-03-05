b2b4b9548a2a9d13d58040d6c4c6e8f5f4ac0a5d
Akash0902 <115146885+Akash0902@users.noreply.github.com>
2026-03-05 15:23:42 +0530

    Update ec2-jfrog.tf

--- terraform/ec2-jfrog.tf
+++ terraform/ec2-jfrog.tf
@@ -1,1 +1,12 @@
+resource "aws_instance" "jfrog_server"{
+  ami = "ami-0f58b397bc5c1f2e8"
+  instance_type = var.instance_type
+  
+  subnet_id = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.devops_sg.id]
 
+  user_data = file("scripts/install-jfrog.sh")
+
+  tags = {
+    Name = "jfrog-artifactory"
+}
